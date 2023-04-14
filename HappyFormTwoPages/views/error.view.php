671ce3a4d7b12efa81e357eea66a38a6df92250b
Amaury Franssen <amauryfranssen@gmail.com>
2023-04-14 03:50:09 +0200

    build(docker): dockerization is finished

--- HappyFormTwoPages/views/error.view.php
+++ HappyFormTwoPages/views/error.view.php
@@ -2,7 +2,7 @@
 
 <div id="error-container">
     <div id="error-container-content">
-        <h1> Erreur 404 </h1>
+        <h1> Erreur </h1>
         <h2><?= $messageError ?></h2>
         <a class="btn-go-home" href="accueil">Retour à l'accueil</a>
         </div>
