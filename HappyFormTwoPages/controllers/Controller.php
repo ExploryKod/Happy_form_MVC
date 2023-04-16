cf8760f1b38b7350e24f2f59edd825f858700df9
Amaury Franssen <amauryfranssen@gmail.com>
2023-04-16 15:54:55 +0200

    feat(controllers): first step in creating new controllers

--- HappyFormTwoPages/controllers/Controller.php
+++ HappyFormTwoPages/controllers/Controller.php
@@ -18,10 +18,8 @@ require_once("models/postData.model.php");
  */
 class MainController
 {
-    private $postData;
-    private $getData;
-//    private ?array $dataPage = [];
-//    private array $methods = ["GET", "POST", "PUT", 'PATCH', 'DELETE', "OPTIONS"];
+    protected $postData;
+    protected $getData;
 
     public function __construct()
     {
@@ -29,22 +27,7 @@ class MainController
         $this->getData = new getData();
     }
 
-//    public function getDataPage(): ?array
-//    {
-//        return $this->dataPage;
-//    }
-//
-//    /**
-//     * @param string|null $controller
-//     * @return array
-//     */
-//    public function setDataPage(?array $dataPage): array
-//    {
-//        $this->dataPage = $dataPage;
-//        return $this;
-//    }
-
-    private function generatePage($data)
+    protected function generatePage($data)
     {
         extract($data);
         ob_start();
@@ -53,43 +36,6 @@ class MainController
         require_once($template);
     }
 
-    public function accueil()
-    {
-        $clients = $this->getData->getDatas();
-
-        $data_page = [
-            "page_description" => "Affichage de la liste des clients",
-            "page_title" => "Liste des clients",
-            "page_css" => ["index.css"],
-            "page_javascript" => ["displayClients.js"],
-            "clients" => $clients,
-            "view" => "views/home.view.php",
-            "template" => "views/common/template.php"
-        ];
-//        $this->setDataPage($data_page);
-        $this->generatePage($data_page);
-    }
-
-    public function page1()
-    {
-
-        $client = $this->getData->getClientData();
-        $this->postData->deleteClientData();
-        $this->postData->CreateClient();
-        $this->postData->modifyClientData();
-
-        $data_page = [
-            "page_description" => "Enregistrer ou modifier un client à l'aide du formulaire",
-            "page_title" => "Formulaire",
-            "page_css" => ["form.css"],
-            "client" => $client,
-            "page_javascript" => ["inputsValidation.js", "openCloseForm.js", "confirmPopUp.js"],
-            "view" => "./views/form.view.php",
-            "template" => "views/common/template.php"
-        ];
-
-        $this->generatePage($data_page);
-    }
 
     public function pageError($messageError)
     {
