2e078252e0b09c7be57acd7799a43e9ed54c882e
Amaury Franssen <amauryfranssen@gmail.com>
2023-04-20 02:29:36 +0200

    feat(routes): optimize router - clarifying routes categories and path error rationalized

--- HappyFormTwoPages/Router/Router.php
+++ HappyFormTwoPages/Router/Router.php
@@ -1,6 +1,11 @@
 <?php
+require_once __DIR__ . '/Router/PathMaker.php';
+$pathMaker = new PathMaker(__DIR__);
+require_once $pathMaker->getFilePath('Controller.php','controllers');
+require_once $pathMaker->getFilePath('CustomerFormController.php','controllers');
+$customerFormController = new CustomerFormController();
 
-class Routeur
+class Router
 {
     private $routes = array();
 
@@ -17,5 +22,20 @@ class Routeur
         } else {
             throw new Exception("La page demandée n'existe pas.");
         }
+    }
+
+    public function handleRoutes(CustomerFormController $errors)  {
+        try {
+            if (empty($_GET['page'])) {
+                    $page = "accueil";
+            } else {
+                $url = explode("/", filter_var($_GET['page'], FILTER_SANITIZE_URL));
+                $page = $url[0];
+            }
+
+            $this->handleRequest($page);
+            } catch (Exception $e) {
+                $errors->pageError($e->getMessage());
+            }
     }
 }
