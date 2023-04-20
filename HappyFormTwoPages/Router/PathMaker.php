468c18b9691ae0df03509c53361027d7f9059de0
Amaury Franssen <amauryfranssen@gmail.com>
2023-04-20 02:29:36 +0200

    feat(routes): optimize router - clarifying routes categories and path error rationalized

---
+++ HappyFormTwoPages/Router/PathMaker.php
@@ -0,0 +1,88 @@
+<?php
+class PathMaker {
+
+    private string $basePath;
+    private string $rootDirectory;
+    private array $fileTypes = [];
+    private array $intermediateDirectories = [ 'controllers' => 'controllers/', 'router' => 'Router/'];
+
+    public function __construct(string $basePath = null) {
+        if (!$basePath) {
+            $basePath = realpath(__DIR__ . '/../');
+        }
+
+        $this->basePath = $basePath;
+        $this->rootDirectory = $basePath;
+
+
+        // Ajouter le répertoire racine et ses parents à la liste des types de fichiers
+        $currentDirectory = $basePath;
+        while (true) {
+            $this->addFileType('php', str_replace($this->rootDirectory . '/', '', $currentDirectory));
+            $parentDirectory = dirname($currentDirectory);
+            if ($parentDirectory == $currentDirectory) {
+                break;
+            }
+            $currentDirectory = $parentDirectory;
+        }
+    }
+
+
+    public function addFileType(string $fileType, string $directoryName) {
+        $this->fileTypes[$fileType] = $directoryName;
+    }
+
+    public function getFilePath(string $fileName, ?string $fileCategory = "", ?string $fileType = null, ?string $baseDirectory = null) : string {
+        if (!$baseDirectory) {
+            $baseDirectory = $this->basePath;
+        }
+        $array_parent_path = [];
+        $array_categories = [];
+        foreach($this->intermediateDirectories as $key => $value) {
+            $array_categories[] = $key;
+            $array_parent_path[] = $value;
+        }
+        if(!in_array($fileCategory, $array_categories) ) {
+            $errorMsg = sprintf('Category de fichier non-reconnu : %s. Voici les catégories: %s. Les chemins possibles: %s.', $fileCategory, implode(',', array_map('htmlspecialchars', $array_categories)), implode('  ', array_map('htmlspecialchars', $array_parent_path)));
+
+            // Log the error message to a file
+            error_log($errorMsg, 3, __DIR__.'/error.log');
+
+            // Throw an exception without the error message
+            throw new Exception('Nous ne trouvons pas la catégorie '. $fileCategory . '. Plus d\'explications dans les logs.');
+
+        }
+
+        foreach($this->intermediateDirectories as $key => $value) {
+
+            if($key == $fileCategory) {
+                $baseDirectory .= DIRECTORY_SEPARATOR . $value;
+            } else  {
+                $baseDirectory .= "";
+            }
+        }
+
+        if (!$fileType) {
+            $filePath = $baseDirectory . DIRECTORY_SEPARATOR . $fileName;
+        } else {
+            if (!isset($this->fileTypes[$fileType])) {
+                throw new Exception('Type de fichier non-reconnu : ' . $fileType);
+            }
+            $filePath = $baseDirectory . DIRECTORY_SEPARATOR . $this->fileTypes[$fileType] . DIRECTORY_SEPARATOR . $fileName;
+        }
+
+        $realPath = realpath($filePath);
+
+        if (!$realPath) {
+            throw new Exception('Fichier non-trouvé: ' . $filePath);
+        }
+
+        $rootDirectory = realpath($this->rootDirectory);
+        $realPath = str_replace($rootDirectory, $this->rootDirectory, $realPath);
+
+        return $realPath;
+    }
+
+}
+
+
--- HappyFormTwoPages/index.php
+++ HappyFormTwoPages/index.php
@@ -1,50 +1,9 @@
 <?php
-require_once("./controllers/Controller.php");
-require_once("./controllers/CustomerFormController.php");
-require_once __DIR__ .'/Router/Router.php';
-
+$pathMaker = new PathMaker(__DIR__);
+require_once $pathMaker->getFilePath('Router.php','router');
+$routeur = new Router();
 $customerFormController = new CustomerFormController();
-$routeur = new Routeur();
-
 $routeur->addRoute("accueil", array($customerFormController, "accueil"));
 $routeur->addRoute("formulaire", array($customerFormController, "page1"));
-
-try {
-    if (empty($_GET['page'])) {
-        $page = "accueil";
-    } else {
-        $url = explode("/", filter_var($_GET['page'], FILTER_SANITIZE_URL));
-        $page = $url[0];
-    }
-
-    $routeur->handleRequest($page);
-} catch (Exception $e) {
-    $customerFormController->pageError($e->getMessage());
-}
-
-
-
-
-
-//try {
-//    if (empty($_GET['page'])) {
-//        $page = "accueil";
-//    } else {
-//        $url = explode("/", filter_var($_GET['page'], FILTER_SANITIZE_URL));
-//        $page = $url[0];
-//    }
-//
-//    switch ($page) {
-//        case "accueil":
-//            $customerFormController->accueil();
-//            break;
-//        case "formulaire":
-//            $customerFormController->page1();
-//            break;
-//        default:
-//            throw new Exception("La page n'existe pas");
-//    }
-//} catch (Exception $e) {
-//    $mainController->pageError($e->getMessage());
-//}
+$routeur->handleRoutes($customerFormController);
 
