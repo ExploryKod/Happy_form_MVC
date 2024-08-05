ea2913a4a87d9e2b1e5667db21ef853c6ceba4eb
Amaury Franssen <amauryfranssen@gmail.com>
2024-08-05 22:55:23 +0200

    feat(onePage): change bdd parameters

--- HappyFormOnePage/data/db_connexion.php
+++ HappyFormOnePage/data/db_connexion.php
@@ -1,12 +1,7 @@
 <?php 
 // Variables clés qui compose ensuite l'objet PDO utilisé pour se connecter à la base de donnée
 // Vous pouvez ici changer les paramètres de mot de passe ou le nom de la base de donnée si importation de celle-ci sous un autre nom. 
-$dbname = getenv('DB_DATABASE') ?? "db";
-$username = getenv('DB_USERNAME') ?? "root";
-$password = getenv('DB_PASSWORD') ?? "root";
-$host = getenv('DB_HOST')?? "localhost";
-
-$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$pdo = new PDO("mysql:host=bu5tpxypiff1u9ftfngr-mysql.services.clever-cloud.com:3306;dbname=bu5tpxypiff1u9ftfngr", "uj4zk0llky0yuakv", "pCXdevFWucPxq9VZ5nm8");
         
      
 
--- HappyFormOnePage/data/displayClients.php
+++ HappyFormOnePage/data/displayClients.php
@@ -1,5 +1,5 @@
 <?php 
-require_once "db_connexion.php";
+require_once __DIR__ .'/db_connexion.php';
 
 // Permet l'accés à toute la donnée des clients de la base de donnée
 $reqDisplayClients = $pdo->prepare("SELECT id, `last_name`, `first_name`, `address`, `telephone`, DATE_FORMAT(meeting_date, '%d/%m/%Y') as meeting, DATE_FORMAT(created, '%d/%m/%Y') as createdDate FROM clients");
